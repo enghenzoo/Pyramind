eb16f674e78688309b8207c55946af2eb28a4697
Noran Fawzy <noranfawzy05@gmail.com>
2025-09-23 02:11:27 +0300

    Add files via upload

---
+++ src/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import { FaRocket } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import Register from "./components/Register";
+function App() {
+  return (
+    <div >
+      <Register />
+  </div>
+  );
+}
+export default App;
