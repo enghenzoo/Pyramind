a9d5879d53ed4f6bffc2b830af78c7e7fbc79469
Noran Fawzy <noranfawzy05@gmail.com>
2025-11-21 22:29:12 +0200

    Update Obstacle.jsx

--- src/pages/Obstacle.jsx
+++ src/pages/Obstacle.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Obstacle.css';
 
+// Obstacle component represents an in-game obstacle
+// It receives position (x, y) and an optional onClick handler
 function Obstacle({ position, onClick }) {
+  
+  // Inline style used to position the obstacle on screen
   const style = {
-    left: `${position.x}px`,
-    bottom: `${position.y}px`,
+    left: `${position.x}px`,   // Horizontal position
+    bottom: `${position.y}px`, // Vertical position
   };
 
+  // Render the obstacle as a div with dynamic style
+  // onClick is optional, can be used to trigger events (like starting a challenge)
   return <div className="obstacle" style={style} onClick={onClick}></div>;
 }
 
